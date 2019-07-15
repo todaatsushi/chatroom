679be4706eef993957283fea6fc75210307eaa40
Atsushi <todaatsushi0@gmail.com>
2019-07-15 17:23:07 +0100

    Migrate models into db, register models in admin

--- api/rooms/models.py
+++ api/rooms/models.py
@@ -9,8 +9,17 @@ class Chatroom(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     last_posted = models.DateTimeField(null=True)
 
+    def __str__(self):
+        return f'{self.name} - ({self.hash})'
+
+    def get_messages(self):
+        return self.messages.objects.order_by('-posted_at')
 
 class Message(models.Model):
-    chatroom = models.ForeignKey('rooms.Chatroom', on_delete=models.CASCADE)
+    chatroom = models.ForeignKey('rooms.Chatroom', on_delete=models.CASCADE, related_name='messages')
     author = models.CharField(max_length=50)
     message = models.TextField()
+    posted_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Message in {self.chatroom.name}({self.chatroom.hash}) by {self.author} - {self.posted_at}'
