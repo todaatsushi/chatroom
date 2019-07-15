c6e601044a4769f817e4b3ab687da741158bceca
Atsushi <todaatsushi0@gmail.com>
2019-07-15 16:44:50 +0100

    Add rooms app to project with functional tests

---
+++ api/rooms/tests/test_functional.py
@@ -0,0 +1,16 @@
+### FUNCTIONAL TESTS
+
+
+## User goes to existing chat room and posts
+# User goes to home page
+# User sees two options - start a new room or enter room hash
+# User enters a room hash and gets redirected to the chat page
+# User sees chats from before
+# User posts in the room
+
+
+
+## User starts a new chat room
+# User goes to home page
+# User sees two options - start a new room or enter room hash
+# User starts a new chat room - sees an empty chat room with the first message as the new hash
