4ab5bc3e09bc2eb9ac9f811d040b2c8c1c513d75
Atsushi <todaatsushi0@gmail.com>
2019-07-16 19:59:02 +0100

    Edit hash field to have max length

--- api/rooms/models.py
+++ api/rooms/models.py
@@ -4,14 +4,15 @@ from django.utils import timezone
 
 def _generate_hash():
     import uuid
-    return uuid.uuid4().hex
+    return str(uuid.uuid4().hex)[:10]
 
 
 class Chatroom(models.Model):
-    hash = models.UUIDField(
+    hash = models.CharField(
         primary_key=True,
         default=_generate_hash,
-        editable=False
+        editable=False,
+        max_length=10,
     )
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(default=timezone.now)
@@ -23,12 +24,32 @@ class Chatroom(models.Model):
     def get_messages(self):
         return self.messages.objects.order_by('-posted_at')
 
+    def as_dict(self):
+        return {
+            'hash': self.hash,
+            'name': self.name,
+            'created_at': self.created_at,
+            'last_posted': self.last_posted,
+        }
+
 
 class Message(models.Model):
-    chatroom = models.ForeignKey('rooms.Chatroom', on_delete=models.CASCADE, related_name='messages')
+    chatroom = models.ForeignKey(
+        'rooms.Chatroom',
+        on_delete=models.CASCADE
+    )
     author = models.CharField(max_length=50)
     message = models.TextField()
     posted_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'Message in {self.chatroom.name}({self.chatroom.hash}) by {self.author} - {self.posted_at}'
+
+    def as_dict(self):
+        return {
+            'id': self.pk,
+            'chatroom': self.chatroom,
+            'author': self.author,
+            'message': self.message,
+            'posted_at': self.posted_at,
+        }
